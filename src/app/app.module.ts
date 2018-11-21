def7cad9cff964f418d56814103090be9ca8fffe
Angelique Taute <Angelique.Taute@britehouse.co.za>
2018-11-21 10:51:14 +0200

    Added new features and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,24 @@ import {RouterModule} from '@angular/router';
 import {HttpModule} from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { NavComponent } from './Nav/Nav.component';
-import { FooterComponent } from './css/Footer/footer.component';
+import { FooterComponent } from './Footer/footer.component';
+import { Tim1Component } from './Tim1/Tim1.component';
+import { ContactUsComponent } from './ContactUs/Contactus.component';
+import { Tim2Component } from './Tim2/Tim2.component';
+import { Tim3Component } from './Tim3/Tim3.component';
+import { Tim4Component } from './Tim4/Tim4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    Tim1Component,
+    Tim2Component,
+    Tim3Component,
+    Tim4Component,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
